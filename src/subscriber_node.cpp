a8e5a9b1a9a56fb01645c65c7046361625ae7a9e
Madhu <vandanapumadhuhasini@gmail.com>
2026-02-02 12:25:37 +0530

    Completed RO2 Subscriber Assignment

--- src/subscriber_node.cpp
+++ src/subscriber_node.cpp
@@ -6,26 +6,26 @@
 
 using std::placeholders::_1;
 
-/*
- * TODO: Create a Class named 'SubscriberNode' that inherits from rclcpp::Node.
- * Requirements:
- * 1. The constructor should name the node "subscriber_node".
- * 2. Create a subscription to topic "/chatter" with message type std_msgs::msg::String.
- * 3. The subscription callback should:
- *    - Log using RCLCPP_INFO: "I heard: 'MESSAGE'" where MESSAGE is the received data
- */
-
 class SubscriberNode : public rclcpp::Node
 {
 public:
     SubscriberNode()
         : Node("subscriber_node")
     {
-        // TODO: Create the subscription here
+        // Create subscription
+        subscription_ = this->create_subscription<std_msgs::msg::String>(
+            "/chatter",
+            10,
+            std::bind(&SubscriberNode::topic_callback, this, _1));
     }
 
 private:
-    // TODO: Define the topic_callback function here
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg)
+    {
+        RCLCPP_INFO(this->get_logger(),
+                    "I heard: '%s'",
+                    msg->data.c_str());
+    }
 
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
